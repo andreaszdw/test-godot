ea6fb2c680d00f74b5ca5fe163585602f9ce9190
andreaszdw <andreaszdw@googlemail.com>
2023-06-06 15:13:28 +0200

    scrolling

--- sidefighter/Asteroid.gd
+++ sidefighter/Asteroid.gd
@@ -1,7 +1,13 @@
 extends RigidBody2D
-
+	
 func _ready():
 	$AnimatedSprite.playing = true
+
+func scale(s):
+	$AnimatedSprite.scale.x = s
+	$AnimatedSprite.scale.y = s
+	$CollisionShape2D.scale.x = s
+	$CollisionShape2D.scale.y = s
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
--- sidefighter/Main.gd
+++ sidefighter/Main.gd
@@ -1,14 +1,16 @@
 extends Node
 
 export(PackedScene) var asteroid_scene
+var rng = RandomNumberGenerator.new()
 
 func new_game():
 	$StartTimer.start()
 	
 func _ready():
-	randomize()
+	rng.randomize()
 	new_game()
 
+# warning-ignore:unused_argument
 func _process(delta):
 	if Input.is_action_pressed("fullscreen"):
 		OS.set_window_fullscreen(!OS.window_fullscreen)
@@ -25,10 +27,11 @@ func _on_AsteroidTimer_timeout():
 	var ast = asteroid_scene.instance()
 	
 	var spawn_loc = get_node("SpawnPath/SpawnPathLocation")
-	spawn_loc.offset = randi()
+	spawn_loc.offset = rng.randi()
 	ast.position = spawn_loc.position
+	ast.scale(rng.randf_range(0.2, 2))
 	
-	var velocity = Vector2(180, 0)
+	var velocity = Vector2(360, 0)
 	ast.linear_velocity = velocity.rotated(3.14159)
 	
 	add_child(ast)
