fa5d00d4e9936c786645e6e1a49d03f42b3f1975
andreaszdw <andreaszdw@googlemail.com>
2023-09-22 09:11:29 +0200

    steer soldier

--- rts/scenes/TestField.gd
+++ rts/scenes/TestField.gd
@@ -3,6 +3,7 @@ extends Node2D
 export(PackedScene) var soldier_scene
 
 var test_soldier
+var target_position = Vector2.ZERO
 
 func _ready():
 	test_soldier = soldier_scene.instance()
@@ -11,17 +12,18 @@ func _ready():
 	add_child(test_soldier)
 
 func _process(delta):
-	pass
+	var direction =  target_position - test_soldier.position
+	test_soldier.rotation_degrees = rad2deg(direction.angle())
 
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton:
 		#print("Mouse Click/Unclick at: ", event.position)
+		target_position = event.position
 		test_soldier.position = Vector2(300, 300)
 		test_soldier.setAnimation("walk")
 	
 	elif event is InputEventMouseMotion:
-		#print("Mouse Motion at: ", event.position)
 		pass
 	# Print the size of the viewport.
 	#print("Viewport Resolution is: ", get_viewport_rect().size)
