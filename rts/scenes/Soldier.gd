ee94c0d5e03641fc4df440465240431ca213cf9b
andreaszdw <andreaszdw@googlemail.com>
2023-09-22 14:59:44 +0200

    movement

--- rts/scenes/Soldier.gd
+++ rts/scenes/Soldier.gd
@@ -1,11 +1,26 @@
 extends Area2D
 
+var speed = 20.0 # pixel per second
+var destination = Vector2.ZERO
+
 func _ready():
-	#$AnimatedSprite.animation = "walk"
 	pass
 
 func _process(delta):
-	pass
+	#print((destination-position).length())
+	if (destination-position).length() > 2:
+		var direction = destination - position
+		rotation_degrees = rad2deg((destination - position).angle())
+		$AnimatedSprite.animation = "walk"
+		position += direction.normalized() * speed * delta
+	else:
+		$AnimatedSprite.animation = "stand"
 	
 func setAnimation(ani):
 	$AnimatedSprite.animation = ani
+	
+func setDestination(v):
+	destination = v
+
+func setPosition(v):
+	position = v
--- rts/scenes/TestField.gd
+++ rts/scenes/TestField.gd
@@ -3,27 +3,19 @@ extends Node2D
 export(PackedScene) var soldier_scene
 
 var test_soldier
-var target_position = Vector2.ZERO
 
 func _ready():
 	test_soldier = soldier_scene.instance()
-	test_soldier.position.x = 100
-	test_soldier.position.y = 100 
+	test_soldier.setPosition(Vector2(640, 400))
 	add_child(test_soldier)
 
 func _process(delta):
-	var direction =  target_position - test_soldier.position
-	test_soldier.rotation_degrees = rad2deg(direction.angle())
+	pass
 
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton:
-		#print("Mouse Click/Unclick at: ", event.position)
-		target_position = event.position
-		test_soldier.position = Vector2(300, 300)
-		test_soldier.setAnimation("walk")
+		test_soldier.setDestination(event.position)
 	
 	elif event is InputEventMouseMotion:
 		pass
-	# Print the size of the viewport.
-	#print("Viewport Resolution is: ", get_viewport_rect().size)
