69ed75c5a1d8765c26759d54d6a20b4ec53ae18e
andreaszdw <andreaszdw@googlemail.com>
2023-11-15 13:36:12 +0100

    test map

--- tilemap-test/GameScreen.gd
+++ tilemap-test/GameScreen.gd
@@ -21,48 +21,49 @@ func _ready():
 	max_scroll.y = used_rect.size.y * tilemap.tile_set.tile_size.y	
 	
 func _process(delta):
+	print(Engine.get_frames_per_second())
 	var mouse_pos = get_viewport().get_mouse_position()
 	var scroll = Vector2(0, 0)
-	
+
 	# scrolling x, faster and slower
 	if mouse_pos.x < 40:
 		scroll.x = -scroll_speed * delta
 
 	if mouse_pos.x < 15 and mouse_pos.x >= 0:
 		scroll.x = -scroll_speed * 3 * delta
-	
+
 	if mouse_pos.x > window_width - 40:
 		scroll.x = scroll_speed * delta
-			
+
 	if mouse_pos.x > window_width - 15 and mouse_pos.x <= window_width:	
 		scroll.x = scroll_speed * 3 * delta
-	
+
 	# scrolling y, faster and slower
 	if mouse_pos.y < 40:
 		scroll.y = -scroll_speed * 3 * delta
-		
+
 	if mouse_pos.y < 15 and mouse_pos.y >= 0:
 		scroll.y = -scroll_speed * delta
-		
+
 	if mouse_pos.y > window_height - 40:
 		scroll.y = scroll_speed * 3 * delta
-		
+
 	if mouse_pos.y > window_height - 15 and mouse_pos.y <= window_height:
 		scroll.y = scroll_speed * delta
-	
+
 	# scroll it
 	$MapCam.position += scroll	
-	
+
 	# check srolling borders
 	if $MapCam.position.x < 0:
 		$MapCam.position.x = 0
-	
+
 	if $MapCam.position.x > max_scroll.x:
 		$MapCam.position.x = max_scroll.x
-		
+
 	if $MapCam.position.y < 0:
 		$MapCam.position.y = 0
-	
+
 	if $MapCam.position.y > max_scroll.y:
 		$MapCam.position.y = max_scroll.y
 	
